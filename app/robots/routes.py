faf5203be14f18d8bb8c88a935655d0d35df33a0
JulioSanchezD <julioe.sanchezd@gmail.com>
2020-03-06 17:04:54 -0600

    Added request method validator

--- app/robots/routes.py
+++ app/robots/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 robots = Blueprint('robots', __name__)
 
 
-@robots.route("/pcmiler")
+@robots.route("/pcmiler", methods=['GET', 'POST'])
 def pcmiler():
+    if request.method == 'POST':
+        return jsonify({"result": request.get_array(field_name='input_data')})
     return render_template('pcmiler.html', title="Olimpus: PC Miler")
-    # TODO check methods
