c18c3940248420036ee4e788137668c7e724489e
JulioSanchezD <julioe.sanchezd@gmail.com>
2020-03-10 12:48:07 -0600

    Added temporal secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = "algo que cambiaremos depsues martin"
+
     from app.main.routes import main
     from app.robots.routes import robots
 
